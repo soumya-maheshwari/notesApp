9c84e8da3bc795d95b4cb4e70ef1f1d1c826b6ff
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-02-26 15:06:38 +0530

    added navbar and footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <p>copyright © 2023</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./navbar.css";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>NOTES APP</h1>
+    </div>
+  );
+};
+
+export default Navbar;
